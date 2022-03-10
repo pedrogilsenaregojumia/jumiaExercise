cd15aff2ac85b032bb75c427303aaf6acfe376b7
pedro <pedro.srego.ext@jumia.com>
2022-03-10 18:45:23 +0000

    a

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,13 +1,15 @@
 import { cyan } from "@mui/material/colors";
 
 describe("renders them homepage", () => {
-  it("renders correctly", () => {
+  beforeEach(() => {
     cy.visit("/view=full&sort_by=tasks&by_order=desc&per_page=10");
+  });
+
+  it("renders correctly", () => {
     cy.get("#container").should("exist");
   });
 
   it("allows the search and sorting to be used", () => {
-    cy.visit("/view=full&sort_by=tasks&by_order=desc&per_page=10");
     /* ==== Generated with Cypress Studio ==== */
     cy.get("#mui-1").clear();
     cy.get("#mui-1").type("teste");
@@ -21,8 +23,31 @@ describe("renders them homepage", () => {
     cy.get('[element="category"] > b').click();
     cy.get(".css-1ygcj2i-MuiTableCell-root > b").click();
     /* ==== Generated with Cypress Studio ==== */
-    cy.get('#mui-1').clear();
-    cy.get('.makeStyles-search-5').click();
+    cy.get("#mui-1").clear();
+    cy.get(".makeStyles-search-5").click();
+    /* ==== End Cypress Studio ==== */
+  });
+
+  it("finds the task 'Ir àsassasa compras'", () => {
+    cy.findByText("Ir àsassasa compras").should("exist");
+  });
+
+  it("routes to the correct endpoint", () => {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('[element="details"] > b').click();
+    /* ==== End Cypress Studio ==== */
+    cy.url().should("include", "sort_by=details");
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('[element="category"] > b').click();
+    cy.url().should("include", "sort_by=category");
+    cy.get(".css-1ygcj2i-MuiTableCell-root > b").click();
+    cy.url().should("include", "sort_by=tasks");
+    /* ==== End Cypress Studio ==== */
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get("#mui-1").clear();
+    cy.get("#mui-1").type("teste2");
+    cy.get(".makeStyles-search-5").click();
+    cy.url().should("include", "&search_by=teste2");
     /* ==== End Cypress Studio ==== */
   });
 });
